faf6b5a1dabb26dc89da129f8972031fa5d6ff5c
Dinushi Dhananjani <dinushidhananjani@gmail.com>
2021-07-17 18:08:47 +0530

    footer basic

--- src/App.js
+++ src/App.js
@@ -1,29 +1,12 @@
 import logo from "./logo.svg";
 import "./App.scss";
 import { DefaultButton, DefaultCard } from "./components/index";
+import Footer from "../src/App/Footer/Footer";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <DefaultCard>
-          <h1>Test card heading</h1>
-          <p>test card content</p>
-        </DefaultCard>
-        <DefaultButton>Test</DefaultButton>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/App/Footer/Footer.js
@@ -0,0 +1,18 @@
+import { Button, Row, Col } from "antd";
+import React from "react";
+import classNames from "./Footer.module.scss";
+
+const Footer = () => {
+    return (
+        <div className={classNames.footer}>
+            <div className={classNames.footerWrapper}>
+
+            </div>
+            
+        
+
+        </div>
+    )
+}
+
+export default Footer
